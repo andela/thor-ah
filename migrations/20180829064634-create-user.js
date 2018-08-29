29d2b0f85370f02006fc79a83db1777e6eabe37f
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2018-08-29 14:37:34 +0100

    chore(postgres) add sequelize model
    - generate user model with sequelize
    - setup user model with the same interface from previous mongoose schema
    - add user model migrations

---
+++ migrations/20180829064634-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      bio: Sequelize.STRING,
+      image: Sequelize.STRING,
+      hash: Sequelize.STRING,
+      salt: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
