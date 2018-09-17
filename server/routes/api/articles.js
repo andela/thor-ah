64f5b4efc8c0e3e75e13cebb14a8e614f5afdd81
Emmanuel Adesile <emma2adesile@gmail.com>
2018-09-17 10:42:44 +0100

    Fix merge conflicts in article.js file

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -19,7 +19,6 @@ router.post('/:article_slug/comments/:commentId/:reaction', authenticateUser, Co
 router.get('/:article_slug/comments/:commentId/reaction_status', authenticateUser, CommentsController.checkReactionStatus);
 router.get('/:article_slug/comments', authenticateUser, CommentsController.getArticleComments);
 router.put('/:article_slug/comments/:commentId', authenticateUser, CommentsController.updateComment);
-router.get('/', authenticateUser, ArticleController.getAll);
 router.get('/', authenticateUser, ArticleController.getAllArticles);
 router.get('/:articleId/reactions', authenticateUser, LikeDislike.getLikesDislikes);
 router.get('/:articleId/reactions/status', authenticateUser, LikeDislike.getReactionStatus);
